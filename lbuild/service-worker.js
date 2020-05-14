39530fba82db205ae9eac49f368a506ac7b233a2
Dylan Hoogduin <dylanhoogduin@Dylans-MacBook-Pro.local>
2020-05-14 19:13:12 +0200

    new live version

--- lbuild/service-worker.js
+++ lbuild/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.dcb2fe4be6b3bd0131c8b5e760f32226.js"
+  "/precache-manifest.0970e07b527fc85070697ff818e16180.js"
 );
 
 workbox.clientsClaim();
@@ -30,5 +30,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute("/index.html", {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
