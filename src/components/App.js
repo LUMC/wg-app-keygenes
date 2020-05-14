ebd26052e1f53f68ee7e8c8b13b949598d298720
Dylan Hoogduin <dylanhoogduin@Dylans-MacBook-Pro.local>
2020-05-14 18:03:03 +0200

    change menu structure

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react'
 
 import {
-    Container,
+    Container, Dropdown,
     Grid,
     Menu,
     Transition
@@ -12,113 +12,148 @@ import history from "../history";
 import Particles from "react-particles-js";
 
 const charPoses = {
-    exit: { opacity: 0, y: 20 },
+    exit: {opacity: 0, y: 20},
     enter: {
         opacity: 1,
         y: 0,
-        delay: ({ charIndex }) => charIndex * 60
+        delay: ({charIndex}) => charIndex * 60
     }
 };
-class App extends Component{
-    renderMenu(){
-        if (this.props.menu.length < 1){
+
+class App extends Component {
+    renderSubMenu(items){
+        if(items.length < 1){
             return null
         }
         return (
-            <Menu inverted  widths={6} className={'mainMenu'}  pointing secondary  >
+            <Dropdown text='More' pointing className='link item'>
+                <Dropdown.Menu>
+                    {
+                        items.map(
+                            (item, key) => (
+                                <Dropdown.Item
+                                        name={item.reference}
+                                        key={`menuItem-${key}`}
+                                        as={Link}
+                                        to={'/' + item.reference}>
+                                        {item.name}
+                                </Dropdown.Item>
+                            )
+                        )
+                    }
+                </Dropdown.Menu>
+            </Dropdown>
+        )
+    }
+    renderMenu() {
+        if (this.props.menu.length < 1) {
+            return null
+        }
+        const subitems = []
+        return (
+            <Menu inverted widths={6} className={'mainMenu'} pointing secondary>
                 <Router history={history}>
                     {
                         this.props.menu.map(
-                            (item, key) => (
-                                <Menu.Item
-                                    name={item.reference}
-                                    key={`menuItem-${key}`}
-                                    as={Link}
-                                    to={'/'+item.reference}>
-                                    {item.name}
-                                </Menu.Item>
-                            ))
+                            (item, key) => {
+                                if(item.subitem){
+                                    subitems.push(item);
+                                    return null
+                                }
+                                return (
+                                    <Menu.Item
+                                        name={item.reference}
+                                        key={`menuItem-${key}`}
+                                        as={Link}
+                                        to={'/' + item.reference}>
+                                        {item.name}
+                                    </Menu.Item>)
+                            })
+                    }
+                    {
+                        this.renderSubMenu(subitems)
                     }
                 </Router>
             </Menu>
         )
     }
+
     render() {
-        return(
+        return (
             <>
-            <div className='appContent' >
-                <div className={'welcome'}>
+                <div className='appContent'>
+                    <div className={'welcome'}>
+                        <Transition
+                            visible={this.props.sloganText}
+                            animation={"fade up"}
+                            duration={{show: 2000}}
+                        >
+                            <div>
+                                <Particles
+                                    className={'particles'}
+                                    params={{
+                                        "particles": {
+                                            "number": {
+                                                "value": 40
+                                            },
+                                            "size": {
+                                                "value": 3
+                                            }
+                                        }
+                                    }}/>
+                            </div>
+                        </Transition>
+                        <Container>
+                            <Grid columns={1}>
+                                <Grid.Row verticalAlign='middle' className={'welcome-box'}>
+                                    <Grid.Column textAlign={'center'}>
+                                        <div className="introName">
+                                            <SplitText initialPose="exit" pose="enter" charPoses={charPoses}>
+                                                {this.props.applicationDetails.title}
+                                            </SplitText>
+                                        </div>
+                                        <div className="sloganText">
+                                            <Transition
+                                                unmountOnHide={false}
+                                                visible={this.props.sloganText}
+                                                animation={"fade up"}
+                                                duration={{show: 1000}}
+                                            >
+                                                <div>
+                                                    {this.props.applicationDetails.sub_title}
+                                                </div>
+                                            </Transition>
+                                        </div>
+                                    </Grid.Column>
+                                </Grid.Row>
+                                <Transition
+                                    unmountOnHide={false}
+                                    visible={this.props.sloganText}
+                                    animation={"fade down"}
+                                    duration={{show: 800}}
+                                >
+                                    <Grid.Row>
+                                        <Grid.Column>
+                                            {this.renderMenu()}
+                                        </Grid.Column>
+                                    </Grid.Row>
+                                </Transition>
+                            </Grid>
+                        </Container>
+                    </div>
                     <Transition
+                        unmountOnHide={false}
                         visible={this.props.sloganText}
                         animation={"fade up"}
-                        duration={{ show:2000 }}
+                        duration={{show: 1000}}
                     >
-                        <div>
-                            <Particles
-                                className={'particles'}
-                                params={{
-                                    "particles": {
-                                        "number": {
-                                            "value": 40
-                                        },
-                                        "size": {
-                                            "value": 3
-                                        }
-                                    }
-                                }} />
+                        <div className={'page-section'}>
+                            <Container>
+                                {this.props.activePageContent}
+                            </Container>
                         </div>
                     </Transition>
-                <Container>
-                    <Grid columns={1} >
-                        <Grid.Row verticalAlign='middle' className={'welcome-box'} >
-                            <Grid.Column textAlign={'center'} >
-                                <div className="introName">
-                                <SplitText initialPose="exit" pose="enter" charPoses={charPoses}>
-                                    {this.props.applicationDetails.title}
-                                </SplitText>
-                                </div>
-                                <div className="sloganText">
-                                    <Transition
-                                        unmountOnHide={false}
-                                        visible={this.props.sloganText}
-                                        animation={"fade up"}
-                                        duration={{ show:1000 }}
-                                    >
-                                        <div>
-                                            {this.props.applicationDetails.sub_title}
-                                        </div>
-                                    </Transition>
-                                </div>
-                            </Grid.Column>
-                        </Grid.Row>
-                        <Transition
-                            unmountOnHide={false}
-                            visible={this.props.sloganText}
-                            animation={"fade down"}
-                            duration={{ show:800 }}
-                        >
-                        <Grid.Row>
-                            <Grid.Column>
-                                {this.renderMenu()}
-                            </Grid.Column>
-                        </Grid.Row>
-                        </Transition>
-                    </Grid>
-                </Container>
                 </div>
-                <Transition
-                    unmountOnHide={false}
-                    visible={this.props.sloganText}
-                    animation={"fade up"}
-                    duration={{ show:1000 }}
-                >
-                <div className={'page-section'}>
-                    <Container>
-                        {this.props.activePageContent}
-                    </Container>
-                </div>
-                </Transition>
-            </div>
             </>
         )
     }
